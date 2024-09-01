9b2682e4232aa72f68b148d262380fa32f6006b4
AFRA <81957946+afra-af@users.noreply.github.com>
2024-09-01 11:16:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
+import langchain_community
 ##
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
